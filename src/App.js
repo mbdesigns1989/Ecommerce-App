40c3b143b3b5e982a40c8e50eb2ba4bdf4d34661
mbdesigns1989 <mbdesigns1989@yahoo.com>
2023-08-02 02:30:56 +0300

    Add basic installation

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.scss";
+
+function App() {
+  const [state, setstate] = useState(initialState)
+
+  return (
+    <div>
+      <h1>Hello World!!!</h1>
+    </div>
+  );
+}
+
+export default App;

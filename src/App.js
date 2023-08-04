06ff497d8f9fb93e40809eaddd34a104bd7ddff4
mbdesigns1989 <mbdesigns1989@yahoo.com>
2023-08-04 23:32:11 +0300

    Add routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.scss";
+import Header from "./components/header/Header"
+import Footer from "./components/footer/Footer"
+import Home from "./pages/home/Home"
+import Contact from "./pages/contact/Contact"
+
 
 function App() {
-  
+
   return (
-    <div>
-      <h1>Hello World!!!</h1>
-    </div>
+    <>
+    <BrowserRouter>
+    <Header/>
+    <Routes>
+      <Route path="/" element={<Home/>}/>
+      <Route path="/contact" element={<Contact/>}/>
+    </Routes>
+    <Footer/>
+    </BrowserRouter>
+    </>
   );
 }
 

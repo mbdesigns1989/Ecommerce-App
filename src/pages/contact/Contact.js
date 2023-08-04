67b7008c1db9818c7b20ecf20b3fd6dcaacc0318
mbdesigns1989 <mbdesigns1989@yahoo.com>
2023-08-04 04:18:16 +0300

    Update app structure

---
+++ src/pages/contact/Contact.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import styles from './Contact.module.scss';
+
+const Contact = () => {
+    return <div></div>;
+}
+
+
+export default Contact;
---
+++ src/pages/olderHistory/olderHistory.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import styles from './olderHistory.module.scss';
+
+const olderHistory = () => {
+    return <div></div>;
+}
+
+
+export default olderHistory;

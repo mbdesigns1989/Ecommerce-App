2f543298613a298c7c131068b5d20235584a87e3
mbdesigns1989 <mbdesigns1989@yahoo.com>
2023-08-05 06:52:05 +0300

    Add Header component

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import styles from './Header.module.scss';
 
 
 const Header = () => {
 
-    return <div><h1>Header</h1></div>;
+    return (
+        <header>
+            <div className={styles.header}>
+          
+            </div>
+        </header>
+    );
 }
 
 export default Header;
